7fc0f34121f769a7978768fd844638e6411281b5
Michael Schmidt <m@ts.to>
2019-09-12 07:14:35 -0700

    added more globals

--- app.js
+++ app.js
@@ -803,7 +803,7 @@ Global(
     TD,TFoot,TH,THead,TR,TT,U,UL,Video,tagMap,
     // supported $ functions
     $attr, $add, $id, css, cssload, $del, EventMouseDownButton, EventKeyDownButton,
-    EventKeyUpButton, EventMouseUpButton,
+    EventKeyUpButton, EventMouseUpButton, Input, Select, TextArea,
     // other
     Global,Register,_PseudoElement,PseudoElementMarkup
 )
